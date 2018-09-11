238f8c6452180b86369e5e9ee1c77b5dba66ffaf
Henperi <henry.izontimi"gmail.com>
2018-09-10 19:48:45 -0700

    Update unit test

--- test/test.js
+++ test/test.js
@@ -1,42 +1,129 @@
 import chai from 'chai';
-import ordersController from '../app/controllers/ordersController';
+import chaiHttp from 'chai-http';
+// import ordersController from '../app/controllers/ordersController';
 
-chai.use(require('chai-http'));
+chai.use(chaiHttp);
 
-const expect = chai.expect;
+const [expect] = [chai.expect];
 
+/**
+ * Test the orders route
+ */
 
-describe('GET /orders', () => {
-  it('should fetch all the orders stored in memory', () => {
-    return chai.request('http://localhost:5000/api/v1/orders').get('/').then((result) => {
-      expect(result.body).to.be.an('object');
+describe('Orders Route Tests', () => {
+  describe('GET /orders', () => {
+    it('should fetch all the orders stored in memory', (done) => {
+      chai
+        .request('http://127.0.0.1:5000/api/v1/orders')
+        .get('/')
+        .end((err, result) => {
+          // console.log('orders:', result.body.orders);
+          if (result.body.orders) {
+            expect(result).to.have.status(200);
+            expect(result.body.message).to.be.equal('Orders found');
+            expect(result.body).to.be.an('object');
+            done();
+          } else {
+            expect(result).to.have.status(404);
+            expect(result.body.message).to.be.equal('No orders were found');
+            expect(result.body).to.be.an('object');
+            done();
+          }
+        });
+      // .catch(err => console.log(err));
     });
   });
-});
 
-describe('POST /orders', () => {
-  it('should create an order and store it in memory', () => {
-    return chai.request('http://localhost:5000/api/v1/orders').post('/').then((result) => {
-      expect(result).to.have.status(400);
-      expect(result.body).to.be.an('object');
+  describe('POST /orders', () => {
+    it('should not create an order if foodId or quantity is missing', (done) => {
+      const newOrder = {
+        foodItems: [{ foodId: '4801ac7c-4f19-4299-b709-aab25de4f088' }],
+      };
+
+      chai
+        .request('http://127.0.0.1:5000/api/v1/orders')
+        .post('/')
+        .send(newOrder)
+        .end((err, result) => {
+          // console.log(result.body);
+          expect(result).to.have.status(400);
+          expect(result.body).to.be.an('object');
+          expect(result.body.reasons).to.be.equal(
+            'Submitted foodItem does not have a valid format. foodId param or quantity param is not defined',
+          );
+          expect(result.body.message).to.be.equal('Order not created');
+          done();
+        });
+      // .catch(err => console.log(err));
+    });
+
+    it.skip('should create an order and store it in memory', (done) => {
+      const newOrder = {
+        foodItems: [{ foodId: '4801ac7c-4f19-4299-b709-aab25de4f088', quantity: 2 }],
+      };
+
+      chai
+        .request('http://127.0.0.1:5000/api/v1/orders')
+        .post('/')
+        .send(newOrder)
+        .end((err, result) => {
+          // console.log(result.body);
+          expect(result).to.have.status(201);
+          expect(result.body).to.be.an('object');
+          expect(result.body.message).to.be.equal('Order created');
+          done();
+        });
+      // .catch(err => console.log(err));
     });
   });
-});
 
-describe('GET /orders/:orderId', () => {
-  it('should fetch one of the orders stored in memory', () => {
-    return chai.request('http://localhost:5000/api/v1/orders').get('/1').then((result) => {
-      expect(result).to.have.status(404);
-      expect(result.body).to.be.an('object');
+  describe('GET /orders/:orderId', () => {
+    it('should not fetch an order when the ordeId is not found in the list of existing orderIds', (done) => {
+      chai
+        .request('http://127.0.0.1:5000/api/v1/orders')
+        .get('/1')
+        .end((err, result) => {
+          expect(result).to.have.status(404);
+          expect(result.body).to.be.an('object');
+          done();
+        });
+    });
+
+    it.skip('should fetch an order when the orderId provided is found in the list of existing orderIds', (done) => {
+      const newOrder = {
+        foodItems: [{ foodId: '4801ac7c-4f19-4299-b709-aab25de4f088', quantity: 2 }],
+      };
+      chai
+        .request('http://127.0.0.1:5000/api/v1/orders')
+        .post('/')
+        .send(newOrder)
+        .end((err, result) => {
+          // console.log(result.body);
+          chai
+            .request('http://127.0.0.1:5000/api/v1/orders')
+            .get(`/${result.body.createdOrder.orderId}`)
+            .end((err2, newResult) => {
+              expect(newResult).to.have.status(201);
+              expect(newResult.body.message).to.be.equal('Order found');
+            });
+          done();
+        });
     });
   });
-});
 
-describe('PUT /orders/:orderId', () => {
-  it('should update the status of one of the orders stored in memory', () => {
-    return chai.request('http://localhost:5000/api/v1/orders').put('/1').then((result) => {
-      expect(result).to.have.status(400);
-      expect(result.body).to.be.an('object');
+  describe('PUT /orders/:orderId', () => {
+    it('it should not update the status of one of the orders when the order status param is not provided', (done) => {
+      chai
+        .request('http://127.0.0.1:5000/api/v1/orders')
+        .put('/1')
+        .end((err, result) => {
+          // console.log(result.body.errors[0].msg);
+          expect(result).to.have.status(400);
+          expect(result.body).to.be.an('object');
+          expect(result.body.errors[0].msg).to.be.equal('order status is required');
+          expect(result.body).to.have.property('errors');
+          done();
+        });
     });
   });
 });
